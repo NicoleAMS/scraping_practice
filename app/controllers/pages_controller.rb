f5f01c069754a5f8cd64f2320dca583e77744cb2
NicoleAMS <nicole.sentis@gmail.com>
2018-06-06 14:56:49 +0100

    scraping multiple pages (pagination) with watir & bug fix with watir scroll

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,22 +15,35 @@ class PagesController < ApplicationController
 
   def esprit_all
     @esprit = []
+    @product_list = []
     @browser = Watir::Browser.new
     @browser.goto('https://www.esprit.co.uk/search?query=organic')
-    @browser.element(css: '#product-tile-list').children.each_with_index do | li, i |
-      li.scroll.to :center
-      i = {}
-      i[:description] = li.element(css: '.product-description').text
-      i[:price] = li.element(css: '.basic-price').text
-      i[:colours] = []
-      li.element(css: '.hc-color-thumb-container').children.each do | colour |
-        i[:colours] << colour.element(css: 'span.hc-tooltip').inner_html
-      end
-      i[:image_url] = li.element(css: '.product-image-container a .product-image-view').attribute_value('src')
-      i[:product_url] = li.element(css: '.product-info-container a').attribute_value('href')
-      @esprit << i
+    @number_of_pages = @browser.element(css: '#pov-paging').children.count - 2
+
+    esprit_cycle_through_list
+    @number_of_pages.times do
+      @browser.element(css: '#pov-paging li.next').click
+      esprit_cycle_through_list
     end
     @browser.close
+  end
+
+  def esprit_cycle_through_list
+    @browser.element(css: '#product-tile-list').children.each_with_index do | li, i |
+      li.element(css: '.product-image-container').scroll.to :bottom
+      i = {}
+      if li.inner_html.include? "hc-color-thumb-container"
+        i[:description] = li.element(css: '.product-description').text
+        i[:price] = li.element(css: '.reduced-price').text || li.element(css: '.basic-price').text
+        i[:colours] = []
+        li.element(css: '.hc-color-thumb-container').children.each do | colour |
+          i[:colours] << colour.element(css: 'span.hc-tooltip').inner_html
+        end
+        i[:image_url] = li.element(css: '.product-image-container a .product-image-view').attribute_value('src')
+        i[:product_url] = li.element(css: '.product-info-container a').attribute_value('href')
+        @esprit << i
+      end
+    end
   end
 
 end
